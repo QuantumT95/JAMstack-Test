8e3fb09d22cbf1540f073703875f0e1837d5ca78
QuantumT95 <tran.charlie@ymail.com>
2018-10-28 00:29:02 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <h1>Hello World!</h1>
-    <p>My name is Charly Tran</p>
+    <p>My name is Charlie Tran</p>
     <p>It's been a while since I've done any web development</p>
     <p>Please let me join this hackathon!</p>
     <Link to="/page-2/">About Me!</Link>
