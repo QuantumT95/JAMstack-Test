b0141841697a40b3a11af742e31e476f979274e4
sechang01 <41515891+sechang01@users.noreply.github.com>
2018-10-28 00:24:52 -0700

    updated greeting
    howdy!

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hello World!</h1>
+    <h1>Howdy World??!</h1>
     <p>My name is Charlie Tran</p>
     <p>It's been a while since I've done any web development</p>
     <p>Please let me join this hackathon!</p>
