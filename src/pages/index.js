d5ad519b68c436dbf2ba617c4afd186f85b14663
sechang01 <41515891+sechang01@users.noreply.github.com>
2018-10-28 00:29:58 -0700

    descriptive
    allow

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,11 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Howdy World??!</h1>
+    <h1>Hello World!</h1>
     <p>My name is Charlie Tran</p>
     <p>It's been a while since I've done any web development</p>
     <p>Please let me join this hackathon!</p>
-    <Link to="/page-2/">About Me!</Link>
+    <Link to="/page-2/">Please learn more about me inside this link!!!</Link>
   </Layout>
 )
 
