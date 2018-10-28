071696a184036d82e7d034570fc444639244b6e5
sechang01 <41515891+sechang01@users.noreply.github.com>
2018-10-28 00:26:35 -0700

    grammar issue
    web to beb

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
   <Layout>
     <h1>Hello World!</h1>
     <p>My name is Charly Tran</p>
-    <p>It's been a while since I've done any web development</p>
+    <p>It's been a while since I've done any beb development</p>
     <p>Please let me join this hackathon!</p>
     <Link to="/page-2/">About Me!</Link>
   </Layout>
